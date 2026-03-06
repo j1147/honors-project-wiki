907407916474514be964a43225ecc03d3864c82b
j1147 <195060359+j1147@users.noreply.github.com>
2026-03-06 00:12:56 -0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import starlightLinksValidator from 'starlight-links-validator';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://j1147.github.io',
+    site: 'https://j1147.github.io/honors-project-wiki',
     integrations: [starlight({
         title: 'rTS Wiki',
         /*favicon: '/favicon.ico',
