ccf41722155f9da81d3562cd84826c070fb906f8
j1147 <195060359+j1147@users.noreply.github.com>
2026-03-06 00:43:12 -0500

    Again and again

--- src/content.config.ts
+++ src/content.config.ts
@@ -2,6 +2,10 @@ import { defineCollection } from 'astro:content';
 import { docsLoader } from '@astrojs/starlight/loaders';
 import { docsSchema } from '@astrojs/starlight/schema';
 
+import { Image } from 'astro:assets';
+
+import badDrawing from '../src/assets/bad drawing.PNG';
+
 export const collections = {
 	docs: defineCollection({ loader: docsLoader(), schema: docsSchema() }),
 };
