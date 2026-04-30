58e502809cd141eacc5804f9bbb38dc09d7c9c7f
j1147 <195060359+j1147@users.noreply.github.com>
2026-04-30 18:16:55 -0400

    Update content.config.ts

--- src/content.config.ts
+++ src/content.config.ts
@@ -5,6 +5,7 @@ import { docsSchema } from '@astrojs/starlight/schema';
 import { Image } from 'astro:assets';
 
 import badDrawing from '../src/assets/baddrawing.png';
+import screenie from '../src/assets/screenie5.PNG';
 
 export const collections = {
 	docs: defineCollection({ loader: docsLoader(), schema: docsSchema() }),
